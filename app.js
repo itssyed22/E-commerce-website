791fd0a1a85a9f7fe3bc8df6757f8fc8703b0cdb
Abdullah <you@example.com>
2024-05-24 21:41:30 +0500

    pizza home website

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-window.onscroll = function() {scrollFunction()};
-
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                document.getElementById("back-to-top").style.display = "block";
-            } else {
-                document.getElementById("back-to-top").style.display = "none";
-            }
-        }
-
-        
-        document.getElementById('back-to-top').onclick = function() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
-
-       
 document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.querySelector(".menu-toggle");
     const nav = document.querySelector("header nav");
 
+    console.log("Menu Toggle Button:", menuToggle);
+    console.log("Navigation Menu:", nav);
+
     menuToggle.addEventListener("click", () => {
+        console.log("Toggle button clicked");
         menuToggle.classList.toggle("open");
         nav.classList.toggle("open");
     });
+
+    document.getElementById('back-to-top').onclick = function() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    };
 });
-
-
 function sendMessage(){
-    alert("Thanks for Contacting us, your message will be replied soon.")
+    alert("Thanks for contacting us,your message will be replied soon..")
 }
